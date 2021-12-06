6cbcc97cd25971e7ba4ee5eff663f24ef3a03812
Sâmara de Souza <saamarasouzaa15@gmail.com>
2021-12-06 13:15:49 -0300

    Implementação das classes base

--- sistema/base/modelo.py
+++ sistema/base/modelo.py
@@ -12,32 +12,38 @@ class Modelo():
         self.__cursor.close()
     
     def obter_todos(self):
+
         instrucao = "SELECT * FROM {tabela}".format(tabela = self.__nome_tabela)
         try:
+
             self.__cursor.execute(instrucao)
             resultado = self.__cursor.fetchall()
             return resultado
         except:
-            print ("Ocorreu um erro ao executar: ", self.__cursor._last_executed)
+            print ("Houve um erro ao executar! ", self.__cursor._last_executed)
             return None
    
     def obter_por_id(self, id):
+
         try:
+
             instrucao = "SELECT * FROM {tabela} WHERE id=%s".format(tabela = self.__nome_tabela)
             self.__cursor.execute(instrucao, id)
             resultado = self.__cursor.fetchone()
             return resultado
         except:
-            print ("Ocorreu um erro ao executar: ", self.__cursor._last_executed)
+            print ("Houve um erro ao executar!: ", self.__cursor._last_executed)
             return None
 
     def excluir_por_id(self, id):
+
         try:
+            
             instrucao = "DELETE FROM {tabela} WHERE id=%s".format(tabela = self.__nome_tabela)
             self.__cursor.execute(instrucao, id)
             return True
         except:
-            print ("Ocorreu um erro ao executar: ", self.__cursor._last_executed)
+            print ("Houve um erro ao executar! ", self.__cursor._last_executed)
             return False
 
     def obter_cursor(self):
