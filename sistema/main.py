520f2c8cdef60f3aa438e2a9449a31bccbed4dfa
Sâmara de Souza <saamarasouzaa15@gmail.com>
2021-12-06 13:24:17 -0300

    Atualização da configuração de auto confirmação

---
+++ sistema/main.py
@@ -0,0 +1,47 @@
+from controladores.controlador_cliente import ControladorCliente
+from controladores.controlador_profissao import ControladorProfissao
+
+def main():
+
+#Cadastro clientes:
+    #controladorCliente = ControladorCliente()
+    #controladorCliente.cadastrar_cliente("00000000111", "Sâmara", "São Paulo", "999478562", 1)
+
+
+#Excluir Clientes:
+    #controladorCliente = ControladorCliente()
+    #controladorCliente.excluir_cliente(1)
+
+
+#Atualizar Clientes:
+    controladorCliente = ControladorCliente()
+    #controladorCliente.atualizar_alterar_cliente(6,"Maria", "11110000001", "Varginha", "940028922", "Programador")
+
+
+#Obter dados cliente:
+    #controladorCliente = ControladorCliente()
+    #cliente = controladorCliente.obter_dados_cliente(1)
+    #print(cliente)
+
+#Listagem de todos clientes:
+    #controladorCliente = ControladorCliente()
+    #todos = controladorCliente.listar_todo_cliente()
+    #print(todos)
+
+
+#Cadastrar profissão:
+    #controladorProfissao = ControladorProfissao()
+    #controladorProfissao.cadastrar_profissao("Psicólogo")
+
+
+#Excluir Profissão:
+    #controladorProfissao = ControladorProfissao()
+    #controladorProfissao.excluir_profissao(4)
+
+
+#Atualizar profissão:
+    #controladorProfissao = ControladorProfissao()
+    #controladorProfissao.atualizar_alterar_profissao(1, "Programador")
+
+
+main()
